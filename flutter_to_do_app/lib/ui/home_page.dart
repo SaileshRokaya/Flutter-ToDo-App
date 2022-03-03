b76924588553dfabe1e7d38d2a2b8bffdfb468ea
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-03-04 00:19:49 +0545

    dynamic dark theme and light theme mode changes.

---
+++ flutter_to_do_app/lib/ui/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_app/services/theme_services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: Column(
+        children: [
+          Text(
+            "Theme Data",
+            style: TextStyle(fontSize: 30),
+          )
+        ],
+      ),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () {
+          ThemeServices().switchTheme();
+        },
+        child: Icon(
+          Icons.nightlight_round,
+          size: 20,
+        ),
+      ),
+      actions: [
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        ),
+      ],
+    );
+  }
+}
---
+++ flutter_to_do_app/lib/ui/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color bluishClr = Color(0xFF4e5ae8);
+const Color yellowClr = Color(0xFFFFB746);
+const Color pinkClr = Color(0xFFff4667);
+const Color white = Color(0xFFFFFFFFFF);
+const Color primaryClr = bluishClr;
+const Color darkGreyClr = Color(0xFF121212);
+const Color darkHeaderClr = Color(0xFF424242);
+
+class Themes {
+  static final light =
+      ThemeData(primaryColor: primaryClr, brightness: Brightness.light);
+
+  static final dark =
+      ThemeData(primaryColor: darkGreyClr, brightness: Brightness.dark);
+}
